2c6747d519a1d291a1988615d0be803a277de186
Jayoung Lee <39401563+jayounglee92@users.noreply.github.com>
2020-04-22 19:24:01 +0900

    work -ing

---
+++ build/precache-manifest.78c3988d6943be1dcf81b51728531b26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab3b41e9c8d3cde86d7eea7426e61e0",
+    "url": "/jayounglee/index.html"
+  },
+  {
+    "revision": "f51f5e2c249e5c493343",
+    "url": "/jayounglee/static/css/main.31a9a85a.chunk.css"
+  },
+  {
+    "revision": "6ebc1c91dca044d1591a",
+    "url": "/jayounglee/static/js/2.58dced82.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/jayounglee/static/js/2.58dced82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51f5e2c249e5c493343",
+    "url": "/jayounglee/static/js/main.f39a29a5.chunk.js"
+  },
+  {
+    "revision": "66dc31b69fdbfbc69b68",
+    "url": "/jayounglee/static/js/runtime-main.f8d9e64e.js"
+  }
+]);
--- src/component/Work.js
+++ src/component/Work.js
@@ -6,6 +6,35 @@ class Work extends Component {
     render() {
         return (
             <main className="work">
+                <div className="work-con">
+                    <ul className="work-list">
+                        <li className="work-list__item">
+                           <div class="work-list__screen">
+                            <video poster="https://nykim.net/images/posters/poster-2.png" playsinline="" autoplay="" loop="" muted="">
+                                <source data-src="https://nykim.net/videos/video-design.mp4" type="video/mp4" src="https://nykim.net/videos/video-design.mp4"/>
+                                    (앗, 비디오가 잘 나오지 않는 모양이에요. 다른 브라우저로 접속해보시겠어요?)
+                                </video>
+                            </div> 
+                            <div class="work-list__text">
+                                <strong className="work-list__title">Mobiconnect API Index</strong>
+                                <a className="work-list__gotosite" href="https://www.mobiconnect.co.kr/api/" target="_blank" rel="noopener noreferrer" title="Go to Site">Go to Site</a>
+                                <p className="work-list__desc">
+                                    Site providing API key for an international marketplace platform where global mobile traffic all around the world comes together.<br/>
+                                </p>
+                                <div className="work-list__ability">
+                                    <span># HTM</span>
+                                    <span># CSS</span>
+                                    <span># jQuery</span><br/>
+                                    <span># BootStrap</span>
+                                    <span># highlight.js</span><br/>
+                                    <span># GitLab</span>
+                                    <span># Netlify</span>
+                                </div>
+                                
+                            </div>
+                        </li>
+                    </ul>
+                </div>
                 {/* <nav class="index-navigation" role="navigation">
                     <a class="index-nav-link index-all-link link-next-up" href="/" data-ajax-loader="ajax-loader-binded">All</a>
                     <a class="index-nav-link project-collection index-page-transition-link" href="/luscious" data-ajax-loader="ajax-loader-binded">Luscious</a>
@@ -24,7 +53,7 @@ class Work extends Component {
                     <a class="index-nav-link project-collection index-page-transition-link" href="/growing-up-super-model" data-ajax-loader="ajax-loader-binded">Growing Up Supermodel - Main Title</a>
                     <a class="index-nav-link project-collection active-link index-page-transition-link" href="/tdg-logo-animation" data-ajax-loader="ajax-loader-binded">TDG Logo Animation</a>
                 </nav> */}
-                <div className="work-con">
+                {/* <div className="work-con">
                     <section>
                         <h2>REAL WORK</h2>
                         <ul className="work-list">
@@ -97,7 +126,7 @@ class Work extends Component {
                                 </li>
                             </ul>
                         </section>
-                </div>
+                </div> */}
             </main>
         );
     }
