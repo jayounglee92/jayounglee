40ef285d0e9891e190cf660a1f1cebfc91a785cd
jayounglee92 <lzy6853@gmail.com>
2021-10-05 00:41:10 +0900

    work modal added

--- src/component/WorkItem.js
+++ src/component/WorkItem.js
@@ -1,47 +1,32 @@
 import React from 'react';
 import './WorkItem.css';
-import { TagItem, Maybe } from 'component';
 
-const WorkItem = ({ title = '', desc = '', images = [''], url = '', tags = [] }) => {
-  const alertCanNotAccess = (e, url) => {
+const WorkItem = ({ id = null, title = '', images = [''], onClick }) => {
+  const clickWorkItem = (e, id) => {
     e.preventDefault();
-    if (url) {
-      window.open(url, '_blank');
-    }
+    onClick(id);
   };
 
   return (
     <li className='work-item'>
-      <div class='work-item__screen'>
-        <img src={process.env.PUBLIC_URL + `/images/${images[0]}`} alt={''} />
-      </div>
-      <div class='work-item__text'>
-        <div className='work-item__header'>
-          <div className='work-item__header-title'>
-            <strong className='work-item__header-title-back'>{title} </strong>
-            <strong className='work-item__header-title-front'>{title}</strong>
-          </div>
-          <Maybe test={url}>
-            <a
-              className='work-item__view-website'
-              href='#'
-              onClick={(e) => alertCanNotAccess(e, url)}
-              target='_blank'
-              rel='noopener noreferrer'
-              title={`${title}사이트 새창으로 이동`}
-            >
-              View this Website
-              <span>
-                <i>→</i>
-              </span>
-            </a>
-          </Maybe>
-        </div>
-        <p className='work-item__desc'>{desc}</p>
-        <div className='work-item__ability'>
-          {tags.map((item) => (
-            <TagItem name={item} />
-          ))}
+      <div className='work-item__screen'>
+        <img src={process.env.PUBLIC_URL + `/images/${images[0]}`} alt={`${title} 이미지`} />
+        <div className='work-item__text'>
+          <strong className='work-item__header'>{title}</strong>
+          <a
+            className='work-item__view-website'
+            href='#'
+            onClick={(e) => clickWorkItem(e, id)}
+            target='_blank'
+            rel='noopener noreferrer'
+            title={`${title}사이트 새창으로 이동`}
+          >
+            MORE DETAILS
+            <span>
+              <i>→</i>
+            </span>
+          </a>
+          {/* <span className='work-item__source'>{source ? `이미지 출처 : ${source}` : ''}</span> */}
         </div>
       </div>
     </li>
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,18 +1,54 @@
-import React from 'react';
-import { WorkItem } from 'component';
+import React, { useState } from 'react';
+import { WorkItem, TagItem } from 'component';
 import './Work.css';
 import workData from 'data/workData';
+
 const Work = () => {
+  const [targetData, setTargetData] = useState(null);
+
+  const openModal = (id) => {
+    setTargetData(workData.data.find((item) => item.id === id));
+  };
+
+  const closeModal = () => {
+    setTargetData(null);
+  };
+
   return (
-    <main className='work'>
-      <div className='work-con'>
-        <ul className='work-list'>
-          {workData.data.map(({ title, desc, images, url, tags }) => (
-            <WorkItem title={title} desc={desc} images={images} url={url} tags={tags} />
-          ))}
-        </ul>
+    <>
+      <div className={`modal-wrap ${targetData ? 'visible' : 'hidden'}`}>
+        <div className='modal-mask' onClick={closeModal}></div>
+        <div className='modal-con'>
+          <div className='modal-img'>
+            <img
+              src={process.env.PUBLIC_URL + `/images/${targetData?.images[0]}`}
+              alt={`${targetData?.title} 이미지`}
+            />
+            <spen onClick={closeModal}>x</spen>
+          </div>
+          <div className='modal-text'>
+            <h3 className='modal-text__title'>{targetData?.title}</h3>
+            <div className='modal-text__ability'>
+              {targetData?.tags.map((item) => (
+                <TagItem name={item} />
+              ))}
+            </div>
+            <hr className='modal-text__hr' />
+            <p className='modal-text__desc'>{targetData?.desc}</p>
+          </div>
+        </div>
       </div>
-    </main>
+      <main className='work'>
+        <div className='work-con'>
+          <ul className='work-list'>
+            {workData.data.map(({ id, title, images }) => (
+              <WorkItem key={id} id={id} title={title} images={images} onClick={openModal} />
+            ))}
+          </ul>
+          <span>이해를 돕기 위한 이미지로 이미지에 있는 데이터는 실제와 상이합니다.</span>
+        </div>
+      </main>
+    </>
   );
 };
 
